ac3568b45be8e9635a9c7134bb9069fa71a495b0
Sergey Nesterovich <blooker@yandex.ru>
2022-07-27 22:31:26 +0300

    2

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+// Здесь функциональность точки входа
 const express = require('express');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
-})
+// Здесь данные
+const users = [
+  { name: 'Мария', age: 22 },
+  { name: 'Виктор', age: 30 },
+  { name: 'Анастасия', age: 48 },
+  { name: 'Алексей', age: 51 }
+];
 
-app.get('/app', (req, res) => {
-  res.send(req.query);
-})
+// Здесь роутинг
+app.get('/users/:id', (req, res) => {
+  if (!users[req.params.id]) {
+    res.send(`Такого пользователя не существует`);
+
+    // не забудем выйти из функции
+    return;
+  }
+
+  const { name, age } = users[req.params.id];
+
+  res.send(`Пользователь ${name}, ${age} лет`);
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
